3522c4ff004f6cd07d906b1bafc6a919796e8b8c
Michaela Schimmele <sm1910@hotmail.de>
2019-03-26 12:33:50 +0100

    label verändert

---
+++ Web_TestGitHub/Erste.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web_TestGitHub
+{
+    public partial class Erste : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
